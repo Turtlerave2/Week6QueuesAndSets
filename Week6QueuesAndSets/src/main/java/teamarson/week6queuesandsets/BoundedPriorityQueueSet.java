4bb74a2daac59ba211a284214fcd447e18c51dc8
leoze <leoze@192.168.1.187>
2023-04-07 13:46:00 +0100

    added the required variables to start of the class, Created Node and a constructor to create HashSet for taskSet

--- Week6QueuesAndSets/src/main/java/teamarson/week6queuesandsets/BoundedPriorityQueueSet.java
+++ Week6QueuesAndSets/src/main/java/teamarson/week6queuesandsets/BoundedPriorityQueueSet.java
@@ -5,14 +5,39 @@
 
 package teamarson.week6queuesandsets;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author leoze
  */
 public class BoundedPriorityQueueSet {
 
-    public static void main(String[] args) {
-        System.out.println("Project to do here!");
+    private int maxSize;
+    private int size;
+    private Node first;
+    private Set<Task> taskSet;
+    
+    
+    public BoundedPriorityQueueSet() {
+        this.maxSize = 10;
+        this.size = 0;
+        this.first = null;
+        this.taskSet = new HashSet<>();
     }
+    
+    
+    
+    private class Node {
+        Task data;
+        Node next;
+
+        Node(Task task) {
+            this.data = task;
+            this.next = null;
+        }
+    }
+    
     
 }
