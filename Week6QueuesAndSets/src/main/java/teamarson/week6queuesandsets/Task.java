1c45cb9a6bf80d0e0534b6fcd433de5585c9fdfc
leoze <leoze@192.168.1.187>
2023-04-07 12:50:08 +0100

    Task class, has been given 3 variables, added constructor, getters and setters, where deadline variable is processed validation is done.

--- Week6QueuesAndSets/src/main/java/teamarson/week6queuesandsets/Task.java
+++ Week6QueuesAndSets/src/main/java/teamarson/week6queuesandsets/Task.java
@@ -4,10 +4,57 @@
  */
 package teamarson.week6queuesandsets;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author leoze
  */
 public class Task {
+
+    private String owner;
+    private String description;
+    private LocalDate deadline;
+
+    public Task(String owner, String description, LocalDate deadline) {
+        if (deadline.compareTo(LocalDate.now()) <= 0) {
+            throw new IllegalArgumentException("Deadline must be after today's date.");
+        }
+        this.owner = owner;
+        this.description = description;
+        this.deadline = deadline;
+    }
+
+    public Task() {
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        if (deadline.compareTo(LocalDate.now()) <= 0) {
+            throw new IllegalArgumentException("Deadline must be after today's date.");
+        }
+        this.deadline = deadline;
+    }
+    
+    
     
 }
