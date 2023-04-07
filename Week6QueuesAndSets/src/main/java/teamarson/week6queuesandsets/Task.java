bcf6117107c798ef32c73db547b01dc742031f7b
leoze <leoze@192.168.1.187>
2023-04-07 13:21:52 +0100

    Added a lot of text and javadocs to the methods of the task class

--- Week6QueuesAndSets/src/main/java/teamarson/week6queuesandsets/Task.java
+++ Week6QueuesAndSets/src/main/java/teamarson/week6queuesandsets/Task.java
@@ -17,6 +17,16 @@ public class Task implements Comparable<Task> {
     private String description;
     private LocalDate deadline;
 
+    /**
+     * Creates a new task with the given owner, description, and deadline.
+     * Validation is done to deadline variable
+     *
+     * @param owner the owner of the task
+     * @param description a description of the task
+     * @param deadline the deadline for the task
+     * @throws IllegalArgumentException if the deadline is before or equal to
+     * today's date
+     */
     public Task(String owner, String description, LocalDate deadline) {
         if (deadline.compareTo(LocalDate.now()) <= 0) {
             throw new IllegalArgumentException("Deadline must be after today's date.");
@@ -26,9 +36,14 @@ public class Task implements Comparable<Task> {
         this.deadline = deadline;
     }
 
+    /**
+     * A no parameter constructor
+     */
     public Task() {
+
     }
 
+    //getters for the task class
     public String getOwner() {
         return owner;
     }
@@ -41,6 +56,7 @@ public class Task implements Comparable<Task> {
         return deadline;
     }
 
+    //setters for the task class
     public void setOwner(String owner) {
         this.owner = owner;
     }
@@ -49,6 +65,7 @@ public class Task implements Comparable<Task> {
         this.description = description;
     }
 
+    //validation is done for the deadline before setting
     public void setDeadline(LocalDate deadline) {
         if (deadline.compareTo(LocalDate.now()) <= 0) {
             throw new IllegalArgumentException("Deadline must be after today's date.");
@@ -56,6 +73,12 @@ public class Task implements Comparable<Task> {
         this.deadline = deadline;
     }
 
+    /**
+     * Returns a hash code value for the Task object based on the values of its
+     * owner, description, and deadline fields.
+     *
+     * @return an integer hash code value for the Task object.
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -65,6 +88,14 @@ public class Task implements Comparable<Task> {
         return hash;
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this one. Tasks are
+     * considered equal if they have the same owner, description, and deadline
+     * fields.
+     *
+     * @param obj the object to compare to.
+     * @return true if the objects are equal, false otherwise.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -86,11 +117,23 @@ public class Task implements Comparable<Task> {
         return Objects.equals(this.deadline, other.deadline);
     }
 
+    /**
+     * Returns a string representation of the task including all variables.
+     *
+     * @return a string representation of the task
+     */
     @Override
     public String toString() {
         return "Task{" + "owner=" + owner + ", description=" + description + ", deadline=" + deadline + '}';
     }
 
+    /**
+     * Compares this task to another task based on their deadlines.
+     *
+     * @param t the task to compare to
+     * @return a negative integer, zero, or a positive integer as this task is
+     * less than, equal to, or greater than the other task
+     */
     @Override
     public int compareTo(Task t) {
         return this.deadline.compareTo(t.getDeadline());
