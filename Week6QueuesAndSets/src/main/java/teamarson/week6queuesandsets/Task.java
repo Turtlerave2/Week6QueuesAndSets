6be4281a8548fc9d0a7036cff98f5cedb75e9e33
leoze <leoze@192.168.1.187>
2023-04-07 13:00:12 +0100

    Added an override hashcode and equals methods to the task class, Added an override toString method.

--- Week6QueuesAndSets/src/main/java/teamarson/week6queuesandsets/Task.java
+++ Week6QueuesAndSets/src/main/java/teamarson/week6queuesandsets/Task.java
@@ -5,6 +5,7 @@
 package teamarson.week6queuesandsets;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 /**
  *
@@ -54,6 +55,42 @@ public class Task {
         }
         this.deadline = deadline;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.owner);
+        hash = 67 * hash + Objects.hashCode(this.description);
+        hash = 67 * hash + Objects.hashCode(this.deadline);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Task other = (Task) obj;
+        if (!Objects.equals(this.owner, other.owner)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return Objects.equals(this.deadline, other.deadline);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + "owner=" + owner + ", description=" + description + ", deadline=" + deadline + '}';
+    }
+    
     
     
     
